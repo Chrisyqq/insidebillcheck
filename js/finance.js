38f6b9159df97ec660cf42e2471bec89f1e7f3f0
Chris <chrisyqq1993@gmail.com>
2016-11-01 17:33:42 +0800

    no message

--- js/finance.js
+++ js/finance.js
@@ -8,5 +8,12 @@
     });
     $('.alertfalse').click(function () {
         $('.finance-alert').css('display','none');
-    })
+    });
+    var mydate = new Date();
+    $('.timechose option').eq(0).text(mydate.getFullYear()+"年"+(mydate.getMonth()-4)+"月");
+    $('.timechose option').eq(1).text(mydate.getFullYear()+"年"+(mydate.getMonth()-3)+"月");
+    $('.timechose option').eq(2).text(mydate.getFullYear()+"年"+(mydate.getMonth()-2)+"月");
+    $('.timechose option').eq(3).text(mydate.getFullYear()+"年"+(mydate.getMonth()-1)+"月");
+    $('.timechose option').eq(4).text(mydate.getFullYear()+"年"+mydate.getMonth()+"月");
+    $('.timechose option').eq(5).text(mydate.getFullYear()+"年"+(mydate.getMonth()+1)+"月");
 })();
